0737454c809617ad1fca3cc60ae932acb1ad7aa3
Leliksey <57276673+Leliksey@users.noreply.github.com>
2024-07-22 12:54:22 +0300

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const modal = require('./components/modal.js');
-const owl = require('./components/owl.js');
-const burger = require('./components/burger.js');
-const oneMoreScript = require('./components/oneMoreScript.js');
-//const uiSlider = require('./components/uiSlider.js');
